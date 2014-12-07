87508298bb145cdc5f5a36c5568dfde77e930730
James Li <jamescfli@yahoo.com>
2014-12-07 12:09:41 +0800

    add function of receiving data from sub-activities with startActivityForResult() and onActivityResult()

--- ActivityTest15ExplicitIntent/src/cn/nec/nlc/example/activitytest15/MainActivity.java
+++ ActivityTest15ExplicitIntent/src/cn/nec/nlc/example/activitytest15/MainActivity.java
@@ -3,14 +3,16 @@ package cn.nec.nlc.example.activitytest15;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	public static final String MSG_KEY = "MsgFromMainActivity";
+	// extra message key for outgoing intent
+	public static final String MSG_KEY_MAIN = "MsgFromMainActivity";
+	// constant to determine which sub-activity returns
+	private static final int REQUEST_CODE = 10;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,11 +30,29 @@ public class MainActivity extends Activity {
 	    Intent i = new Intent(this, ResultActivity.class);
 	    
 	    // pass the value of the EditText view to the sub-activity
-	    i.putExtra(MSG_KEY, value);
+	    i.putExtra(MSG_KEY_MAIN, value);
 
-	    // TODO 2 start second activity with
-	    // startActivity(intent);
-	    startActivity(i);
+//	    // TODO 2 start second activity with
+//	    // startActivity(intent);
+//	    startActivity(i);
+	    
+	    // TODO 2.. now use 
+	    // startActivityForResult(i, REQUEST_CODE);
+	    startActivityForResult(i, REQUEST_CODE);
+	}
+	
+	// TODO 3 Implement this method
+	// assumes that "returnkey" is used as key to return the result
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+	    	if (data.hasExtra(ResultActivity.MSG_KEY_RESULT)) {
+	    		String result = data.getExtras().getString(ResultActivity.MSG_KEY_RESULT);
+	    		if (result != null && result.length() > 0) {
+	    			Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+	    		}
+	    	}
+	    }
 	}
 
 } 
--- ActivityTest15ExplicitIntent/src/cn/nec/nlc/example/activitytest15/ResultActivity.java
+++ ActivityTest15ExplicitIntent/src/cn/nec/nlc/example/activitytest15/ResultActivity.java
@@ -1,11 +1,15 @@
 package cn.nec.nlc.example.activitytest15;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ResultActivity extends Activity {
 
+	public static final String MSG_KEY_RESULT = "MsgFromResultActivity";
+	
 	@Override
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
@@ -15,12 +19,35 @@ public class ResultActivity extends Activity {
 		Bundle extra = getIntent().getExtras();
 		
 		// Get the value of the passed extra with the extras.getString("yourkey")
-		String value = extra.getString(MainActivity.MSG_KEY);
+		String value = extra.getString(MainActivity.MSG_KEY_MAIN);
 		
 		// value should be placed in the TextView with the displayintentextra ID
 		TextView tv = (TextView) findViewById(R.id.displayintentextra);
 		tv.setText(value);
 		
 	}
+	
+	@Override
+	public void finish() {
+	    
+		// TODO 1 create new Intent 
+		// Intent intent = new Intent();
+		Intent i = new Intent();
+	    
+		// TODO 2 read the data of the EditText field
+		// with the id returnValue
+		EditText et = (EditText) findViewById(R.id.returnValue);
+	  
+		// TODO 3 put the text from EditText
+		// as String extra into the intent
+		// use editText.getText().toString();
+		String extra = et.getText().toString();
+		i.putExtra(MSG_KEY_RESULT, extra);
+	    
+		// TODO 4 use setResult(RESULT_OK, intent); 
+		// to return the Intent to the application
+		setResult(RESULT_OK, i);
+		super.finish();
+	} 
 	
 } 
