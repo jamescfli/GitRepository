109ad5d13de326364d2bab440159796b5bd5e60a
James Li <jamescfli@yahoo.com>
2014-12-10 23:00:28 +0800

    Finished lifecycle notification in TracerActivity, both Main and Second Activity are inherited from it.

--- ActivityTest19ActivityLifecycle/src/cn/nec/nlc/example/activitytest19/MainActivity.java
+++ ActivityTest19ActivityLifecycle/src/cn/nec/nlc/example/activitytest19/MainActivity.java
@@ -1,34 +1,20 @@
 package cn.nec.nlc.example.activitytest19;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TracerActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View view) {
+	    Intent intent = new Intent(this, SecondActivity.class);
+	    startActivity(intent);
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-}
+} 
