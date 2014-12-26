ff53c2d5027858975ba1299e726feaa22a764d77
James Li <jamescfli@yahoo.com>
2014-12-26 10:46:58 +0800

    Revise ActivityTest25 SettingsActivity to show current setting value

--- ActivityTest25SharedPreferences/src/cn/nec/nlc/example/activitytest25/SettingsActivity.java
+++ ActivityTest25SharedPreferences/src/cn/nec/nlc/example/activitytest25/SettingsActivity.java
@@ -1,8 +1,13 @@
 package cn.nec.nlc.example.activitytest25;
 
 import cn.nec.nlc.example.sharedpreftest1.R;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 
 public class SettingsActivity extends PreferenceActivity {
@@ -22,13 +27,57 @@ public class SettingsActivity extends PreferenceActivity {
         	(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 	
-	public static class MyPreferenceFragment extends PreferenceFragment {
+	public static class MyPreferenceFragment extends PreferenceFragment 
+		implements OnSharedPreferenceChangeListener {
         @Override
         public void onCreate(final Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.mypreferences);
+            
+            // show the current value in the settings screen
+            for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+            	initSummary(getPreferenceScreen().getPreference(i));
+            }
         }
+        
+        @Override
+        public void onResume() {
+        	super.onResume();
+        	getPreferenceScreen().getSharedPreferences()
+        		.registerOnSharedPreferenceChangeListener(this);
+        }
+        
+        @Override
+        public void onPause() {
+        	super.onPause();
+        	getPreferenceScreen().getSharedPreferences()
+    			.unregisterOnSharedPreferenceChangeListener(this);
+        }
+        
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+        		String key) {
+        	updatePreferences(findPreference(key));
+        }
+
+		private void initSummary(Preference preference) {
+			if (preference instanceof PreferenceCategory) {
+				PreferenceCategory cat = (PreferenceCategory) preference;
+				for (int i = 0; i < cat.getPreferenceCount(); i++) {
+					initSummary(cat.getPreference(i));
+				}
+			} else {
+				updatePreferences(preference);
+			}
+		}
+		
+		private void updatePreferences(Preference findPreference) {
+			if (findPreference instanceof EditTextPreference) {
+				EditTextPreference editTextPref = (EditTextPreference) findPreference;
+				findPreference.setSummary(editTextPref.getText());
+			}
+		}
     }
 	
 }
