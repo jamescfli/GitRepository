f8d4e3cfc40dd72f34c67bda48beaf68f6cf6917
James Li <jamescfli@yahoo.com>
2015-03-13 10:36:04 +0800

    Revise ActivityTest55 to test list AlertDialog

--- ActivityTest55DialogFrag/app/src/main/java/cn/nec/nlc/example/jamesli/activitytest55dialogfrag/FireMissilesDialogFragment.java
+++ ActivityTest55DialogFrag/app/src/main/java/cn/nec/nlc/example/jamesli/activitytest55dialogfrag/FireMissilesDialogFragment.java
@@ -11,24 +11,41 @@ public class FireMissilesDialogFragment extends DialogFragment {
         // empty constructor
     }
 
+//    // Yes - No Dialog
+//    @Override
+//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//        // Use the Builder class for convenient dialog construction
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//        builder.setMessage(R.string.dialog_fire_missiles)
+//                .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        // FIRE ZE MISSILES!
+//                        ((MainActivity) getActivity()).getDisplayTextView().setText("FIRE ZE MISSILES!");
+//                    }
+//                })
+//                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        // User cancelled the dialog
+//                        ((MainActivity) getActivity()).getDisplayTextView().setText("User cancelled the dialog");
+//                    }
+//                });
+//            // one could setNeutralButton as well, at most three buttons
+//        // Create the AlertDialog object and return it
+//        return builder.create();
+//    }
+
+    // List Dialog
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.dialog_fire_missiles)
-                .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE MISSILES!
-                        ((MainActivity) getActivity()).getDisplayTextView().setText("FIRE ZE MISSILES!");
-                    }
-                })
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
-                        ((MainActivity) getActivity()).getDisplayTextView().setText("User cancelled the dialog");
+        builder.setTitle(R.string.pick_color)
+                .setItems(R.array.colors_array, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String[] colors = getResources().getStringArray(R.array.colors_array);
+                        ((MainActivity) getActivity()).getDisplayTextView().setText(colors[i] + " was selected.");
                     }
                 });
-        // Create the AlertDialog object and return it
         return builder.create();
     }
 }
