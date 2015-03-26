ed85c5b711a0cd449f88996fc2bda0150ef22b52
James Li <jamescfli@yahoo.com>
2015-03-26 11:32:13 +0800

    Revise ActivityTest36 check Android step detector existence

--- ActivityTest36SimplePedometer/app/src/main/java/cn/nec/nlc/example/jamesli/activitytest36simplepedometer/MainActivity.java
+++ ActivityTest36SimplePedometer/app/src/main/java/cn/nec/nlc/example/jamesli/activitytest36simplepedometer/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,10 +15,42 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends Activity {
     private TextView textViewStepCounter;
     private TextView textViewDisplayAndroidPedo;
     private SensorManager sensorManager;
+
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            switch(sensorEvent.sensor.getType()) {
+                case Sensor.TYPE_LINEAR_ACCELERATION:
+                    System.arraycopy(sensorEvent.values, 0, accResultVector, 0, 3);
+                    // add new value to the raw data list and derive sma list
+                    accValuesOnZaxis.addNewDataToRawList(accResultVector[2]);   // consider Z-axis only
+                    // check whether peak has appeared
+                    if (accValuesOnZaxis.findPeak()) {
+                        stepCounterPeakDetection++;
+                        // renew textView
+                        textViewStepCounter.setText("Step Conducted: " + stepCounterPeakDetection);
+                    } // otherwise, wait for the next sensor updates
+                    break;
+                case Sensor.TYPE_STEP_DETECTOR:
+                    if (sensorEvent.values[0] == 1.0f) {
+                        stepCounterAndroidNative++;
+                    }
+                    textViewDisplayAndroidPedo.setText("Step Conducted: " + stepCounterAndroidNative);
+                    break;
+                default:
+                    Toast.makeText(MainActivity.this, "Unknown sensor type event", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+            // n.a.
+        }
+    };
 
     private float[] accResultVector;
     private int stepCounterPeakDetection;
@@ -54,7 +87,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onPause() {
         super.onPause();
         // leave the app by unregister sensor listener
-        sensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(sensorEventListener);
     }
 
     @Override
@@ -79,46 +112,30 @@ public class MainActivity extends Activity implements SensorEventListener {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        switch(sensorEvent.sensor.getType()) {
-            case Sensor.TYPE_LINEAR_ACCELERATION:
-                System.arraycopy(sensorEvent.values, 0, accResultVector, 0, 3);
-                // add new value to the raw data list and derive sma list
-                accValuesOnZaxis.addNewDataToRawList(accResultVector[2]);   // consider Z-axis only
-                // check whether peak has appeared
-                if (accValuesOnZaxis.findPeak()) {
-                    stepCounterPeakDetection++;
-                    // renew textView
-                    textViewStepCounter.setText("Step Conducted: " + stepCounterPeakDetection);
-                } // otherwise, wait for the next sensor updates
-                break;
-            case Sensor.TYPE_STEP_DETECTOR:
-                stepCounterAndroidNative++;
-                textViewDisplayAndroidPedo.setText("Step Conducted: " + stepCounterAndroidNative);
-                break;
-            default:
-                Toast.makeText(this, "Unknown sensor type event", Toast.LENGTH_SHORT).show();
-        }
-    }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-        // n.a.
-    }
 
     public void onClickStartButton(View view) {
-        Sensor linearAccSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        sensorManager.registerListener(this, linearAccSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        // Android API 19 add Sensor of "TYPE_STEP_COUNTER"
         Sensor stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-        sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        // notify the user the counting has started
-        Toast.makeText(this, "Step counter started", Toast.LENGTH_LONG).show();
+        if (sensorManager.registerListener(sensorEventListener, stepCounterSensor,
+                SensorManager.SENSOR_DELAY_FASTEST)) {
+            // the sensor is supported
+            // notify the user the counting has started
+            Toast.makeText(this, "Step counter started", Toast.LENGTH_LONG).show();
+        } else {
+            textViewDisplayAndroidPedo.setText("native Step Detector is not supported.");
+        }
+
+        // traditional Pedometer with linear accelerometer sensor
+        Sensor linearAccSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sensorManager.registerListener(sensorEventListener, linearAccSensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+
     }
 
     public void onClickResetButton(View view) {
         // unregister sensor
-        sensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(sensorEventListener);
         // reset textView to original one with 0 step
         textViewStepCounter.setText(getText(R.string.textview_stepcounter_initial));
         textViewDisplayAndroidPedo.setText(getText(R.string.textview_stepcounter_initial));
