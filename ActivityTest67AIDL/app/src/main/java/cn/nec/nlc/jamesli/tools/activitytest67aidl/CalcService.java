37155d23bd95bfab18373606ea6c11d9a8f60136
James Li <jamescfli@yahoo.com>
2015-04-25 12:53:38 +0800

    Add Andriod example NewsReader for reference

---
+++ ActivityTest67AIDL/app/src/main/java/cn/nec/nlc/jamesli/tools/activitytest67aidl/CalcService.java
@@ -0,0 +1,7 @@
+package cn.nec.nlc.jamesli.tools.activitytest67aidl;
+
+/**
+ * Created by jamesli on 15-4-21.
+ */
+public class CalcService {
+}
---
+++ ActivityTest68DisplayBitamps/app/src/main/java/cn/nec/nlc/jamesli/tools/at68displaybitamps/DisLruCache/Streams.java
@@ -0,0 +1,7 @@
+package cn.nec.nlc.jamesli.tools.at68displaybitamps.DisLruCache;
+
+/**
+ * Created by jamesli on 15-4-24.
+ */
+public class Streams {
+}
