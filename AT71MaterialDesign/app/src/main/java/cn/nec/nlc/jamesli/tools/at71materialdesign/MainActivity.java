f8bdd23530bc0be41d50566af2bc87180f695fd5
James Li <jamescfli@yahoo.com>
2015-05-05 19:02:20 +0800

    Upload ActivityTest71 to test CardView in RecyclerView

---
+++ AT71MaterialDesign/app/src/main/java/cn/nec/nlc/jamesli/tools/at71materialdesign/MainActivity.java
@@ -0,0 +1,79 @@
+package cn.nec.nlc.jamesli.tools.at71materialdesign;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.transition.Explode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+
+
+public class MainActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = {"Card 1", "Card 2", "Card 3", "Card 4", "Card 5", "Card 6", "Card 7"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // enable transitions
+        //  getWindow(): Retrieve the current Window for the activity
+        //  requestFeature: Enable extended screen features. This must be called before setContentView().
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+          // FEATURE_CONTENT_TRANSITIONS:
+          // Flag for requesting that window content changes should be animated using a TransitionManager.
+        setContentView(R.layout.activity_main);
+
+        // CardView widgets can have shadows and rounded corners.
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_main_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyMainAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void onSomeButtonClicked(View view) {
+        // setExitTransition()
+        // Sets the Transition that will be used to move Views out of the scene when starting a new Activity.
+        getWindow().setExitTransition(new Explode());
+          // Transition -> Visibility -> Explode
+        Intent intent = new Intent(this, MyOtherActivity.class);
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ AT71MaterialDesign/app/src/main/java/cn/nec/nlc/jamesli/tools/at71materialdesign/MyOtherActivity.java
@@ -0,0 +1,58 @@
+package cn.nec.nlc.jamesli.tools.at71materialdesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyOtherActivity extends Activity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = {"Row 1", "Row 2", "Row 3", "Row 4", "Row 5", "Row 6", "Row 7"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_other);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_other_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyOtherAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_other, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
