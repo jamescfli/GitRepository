637c5138e50d28cc32dd8f677416627d0cdbf02b
James Li <jamescfli@yahoo.com>
2015-05-12 22:42:19 +0800

    Revise AT74 sketch to test AlarmManager

--- AT74ScheduleRepeatAlarm/app/src/main/java/cn/nec/nlc/jamesli/tools/at74schedulerepeatalarm/MainActivity.java
+++ AT74ScheduleRepeatAlarm/app/src/main/java/cn/nec/nlc/jamesli/tools/at74schedulerepeatalarm/MainActivity.java
@@ -1,16 +1,24 @@
 package cn.nec.nlc.jamesli.tools.at74schedulerepeatalarm;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
 
+import java.util.Calendar;
+
 
 public class MainActivity extends ActionBarActivity {
-    PowerManager.WakeLock wakeLock;
+//    private PowerManager.WakeLock wakeLock;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +43,55 @@ public class MainActivity extends ActionBarActivity {
 //        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
 //                "MyWakelockTag");
 //        wakeLock.acquire();
+
+        // 1) ELAPSED_REALTIME_WAKEUP
+        // Wake up the device to fire the alarm in 30 minutes, and every 30 minutes after that
+        alarmMgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                AlarmManager.INTERVAL_HALF_HOUR, AlarmManager.INTERVAL_HALF_HOUR, alarmIntent);
+        // alarm in one minute
+        alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 60*1000,
+                alarmIntent);
+
+        // 2) RTC
+        // difference between setInexactRepeating (approximately) and setRepeating (precise)
+        // setInexactRepeating was recommended to save handset power: When you use setInexactRepeating(),
+        // Android synchronizes repeating alarms from multiple apps and fires them at the same time.
+        // This reduces the total number of times the system must wake the device, thus reducing drain
+        // on the battery.
+        // a) Set the alarm to start at approximately 2:00 p.m.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 14);
+        // With setInexactRepeating(), you have to use one of the AlarmManager interval
+        // constants -- in this case, AlarmManager.INTERVAL_DAY
+        alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, alarmIntent);
+        // b) Wake up the device to fire the alarm at precisely 8:30 a.m., and every 20 minutes thereafter
+        alarmMgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+        // Set the alarm to start at 8:30 a.m.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 8);
+        calendar.set(Calendar.MINUTE, 30);
+        // setRepeating() lets you specify a precise custom interval--in this case,
+        // 20 minutes.
+        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                1000 * 60 * 20, alarmIntent);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 //        wakeLock.release();
+        // cancel alarm
+        if (alarmMgr != null) {
+            alarmMgr.cancel(alarmIntent);
+        }
     }
 
     @Override
