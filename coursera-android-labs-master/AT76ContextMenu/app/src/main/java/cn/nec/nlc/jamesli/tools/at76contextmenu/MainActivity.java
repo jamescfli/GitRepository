049bd9dae714a64647cde1003f8b9445a14a5061
James Li <jamescfli@yahoo.com>
2015-06-01 11:51:11 +0800

    Add coursera labs directory to this repository

---
+++ coursera-android-labs-master/AT76ContextMenu/app/src/main/java/cn/nec/nlc/jamesli/tools/at76contextmenu/MainActivity.java
@@ -0,0 +1,68 @@
+package cn.nec.nlc.jamesli.tools.at76contextmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        registerForContextMenu(tv);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Create Context Menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    // Process clicks on Context Menu Items
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.help_guide:
+                Toast.makeText(getApplicationContext(), "ContextMenu Shown",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+}
